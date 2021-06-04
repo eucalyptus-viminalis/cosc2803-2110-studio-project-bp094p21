fb15d11b3633068f5b39b51691c67758d7d1adab
Lachlan <s3896004@student.rmit.edu.au>
2021-06-04 15:17:26 +1000

    SmallEdit

--- src/main/java/app/Infections.java
+++ src/main/java/app/Infections.java
@@ -2,8 +2,6 @@ package app;
 
 import java.util.ArrayList;
 
-import org.graalvm.compiler.nodes.gc.G1PostWriteBarrier;
-
 import io.javalin.http.Context;
 import io.javalin.http.Handler;
 
@@ -62,7 +60,7 @@ public class infections implements Handler {
             infectionsvar = infectionsvar + doNothing();
         }
         else {
-            infectionsvar = infectionsvar + outputDate1(date1_textbox);
+            infectionsvar = infectionsvar + outputDate(date1_textbox, date2_textbox);
         }
         infectionsvar = infectionsvar + "</body>" + "</html>";
 // DO NOT MODIFY THIS
@@ -71,7 +69,7 @@ public class infections implements Handler {
     }
     public String doNothing() {
         String infectionsvar = "";
-        infectionsvar = infectionsvar + "<h2>COVID-19 Data</h2>";
+        infectionsvar = infectionsvar + "<h2>COVID-19 Default Data</h2>";
 
         JDBCConnection jdbc = new JDBCConnection();
         ArrayList<String> covid = jdbc.getDefaultData();
@@ -99,19 +97,11 @@ public class infections implements Handler {
         return infectionsvar;
     }
 
-    public String outputDate1(String date1) {
+    public String outputDate(String date1, String date2) {
         String infectionsvar = "";
-    }
-
-    public String outputDate2(String date2) {
-
-    }
-
-    public String outputShallowOption(String option) {
-
-    }
-
-    public String outputCountries(String fin) {
-
+        infectionsvar = infectionsvar + "<h2>COVID-19 Data Between 2 Dates</h2>";
+        
+        
+        return infectionsvar;
     }
 }
