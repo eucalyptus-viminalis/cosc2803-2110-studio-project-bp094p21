1f821b6beccc9f2be9bf8748f0c950f78eb7e5f4
Fibre P <fttx5525@gmail.com>
2021-06-10 14:13:29 +1000

    Add content to similar.html

--- src/main/java/app/JDBC2.java
+++ src/main/java/app/JDBC2.java
@@ -74,6 +74,7 @@ public class JDBC2 {
         }
         return str_list;
     }
+
     public int getPastYearTotalCasesCountry() {
         int sum = 0;
         Connection connection = null;
@@ -407,6 +408,278 @@ public class JDBC2 {
             }
         }
         return global_data;
+    }   
+
+    // Similar
+    public ArrayList<String> getUSStateNames() {
+        ArrayList<String> str_list = new ArrayList<String>();
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(DATABASE2);
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30);
+            String query = "select state.name n from state join country on state.countryid = country.id where country.name='US' order by state.name asc";
+            ResultSet results = statement.executeQuery(query);
+            while (results.next()) {
+                String name = results.getString("n");
+                str_list.add(name);
+            }
+            statement.close();
+        }
+        catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        finally {
+            try{
+                if (connection != null) {
+                    connection.close();
+                }
+            }
+            catch (SQLException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+        return str_list;
+    }
+    public ArrayList<String> get3SimilarCasesCountryMil (String country) {
+        ArrayList<String> similar_data = new ArrayList<String>();
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(DATABASE2);
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30);
+            String query = "select name n, printf('%.4f', (1.0*sum(newcases))/(1.0 * population/1000000)) [permil], printf('%.4f', abs((select (1.0*sum(newcases))/(1.0 * population/1000000) from countryrecords join country on country.id = countryrecords.countryid group by name) - (1.0*sum(newcases))/(1.0 * population/1000000))) difference from countryrecords join country on country.id = countryrecords.countryid where name is not '" + country + "' group by name order by abs((select (1.0*sum(newcases))/(1.0 * population/1000000) from countryrecords join country on country.id = countryrecords.countryid where name = '" + country + "' group by name) - (1.0*sum(newcases))/(1.0 * population/1000000)) asc limit 3";
+            ResultSet results = statement.executeQuery(query);
+            while (results.next()) {
+                String name     = results.getString("n");
+                String casespermil = results.getString("permil");
+                similar_data.add(name);
+                similar_data.add(casespermil);
+            }
+            statement.close();
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+        return similar_data;
+    }   
+    public ArrayList<String> get3SimilarCasesUSStateMil (String state) {
+        ArrayList<String> similar_data = new ArrayList<String>();
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(DATABASE2);
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30);
+            String query = "select state.name n, printf('%.4f', (1.0*sum(newcases))/(1.0 * state.population/1000000)) [permil], printf('%.4f', abs((select (1.0*sum(newcases))/(1.0 * state.population/1000000) from staterecords join state on state.id = staterecords.stateid join country on country.id = state.countryid group by state.name) - (1.0*sum(newcases))/(1.0 * state.population/1000000))) difference from staterecords join state on state.id = staterecords.stateid join country on country.id = state.countryid where state.name is not '" + state + "' and country.name = 'US' group by state.name having sum(newcases) > 0 order by abs((select (1.0*sum(newcases))/(1.0 * state.population/1000000) from staterecords join state on state.id = staterecords.stateid where name = '" + state + "' group by name) - (1.0*sum(newcases))/(1.0 * state.population/1000000)) asc limit 3";
+            ResultSet results = statement.executeQuery(query);
+            while (results.next()) {
+                String name     = results.getString("n");
+                String casespermil = results.getString("permil");
+                similar_data.add(name);
+                similar_data.add(casespermil);
+            }
+            statement.close();
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+        return similar_data;
+    }   
+    public ArrayList<String> get3SimilarRatioCountry (String country) {
+        ArrayList<String> similar_data = new ArrayList<String>();
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(DATABASE2);
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30);
+            String query = "select name, printf('%.2f', (100.0 * sum(newdeaths)/sum(newcases))) ratio, abs((select (100.0 * sum(newdeaths)/sum(newcases)) from countryrecords join country on country.id = countryrecords.countryid group by name) - (100.0 * sum(newdeaths)/sum(newcases))) difference from countryrecords join country on country.id = countryrecords.countryid where name is not '" + country + "' group by name order by abs((select (100.0 * sum(newdeaths)/sum(newcases)) from countryrecords join country on country.id = countryrecords.countryid where name = '" + country + "' group by name) - (100.0 * sum(newdeaths)/sum(newcases))) asc limit 3";
+            ResultSet results = statement.executeQuery(query);
+            while (results.next()) {
+                String name     = results.getString("name");
+                String ratio = results.getString("ratio");
+                similar_data.add(name);
+                similar_data.add(ratio);
+            }
+            statement.close();
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+        return similar_data;
+    }   
+    public ArrayList<String> get3SimilarRatioUSState (String state) {
+        ArrayList<String> similar_data = new ArrayList<String>();
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(DATABASE2);
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30);
+            String query = "select state.name n, printf('%.2f', (100.0 * sum(newdeaths)/sum(newcases))) ratio, abs((select (100.0 * sum(newdeaths)/sum(newcases)) from staterecords join state on state.id = staterecords.stateid group by state.name) - (100.0 * sum(newdeaths)/sum(newcases))) difference from staterecords join state on state.id = staterecords.stateid join country on country.id = state.countryid where state.name is not '" + state + "' and country.name = 'US' group by state.name having sum(newcases) > 0 order by abs((select (100.0 * sum(newdeaths)/sum(newcases)) from staterecords join state on state.id = staterecords.stateid where state.name = '" + state + "' group by name) - (100.0 * sum(newdeaths)/sum(newcases))) asc limit 3";
+            ResultSet results = statement.executeQuery(query);
+            while (results.next()) {
+                String name     = results.getString("n");
+                String ratio = results.getString("ratio");
+                similar_data.add(name);
+                similar_data.add(ratio);
+            }
+            statement.close();
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+        return similar_data;
+    }   
+    public ArrayList<String> get3SimilarPeakCasesCountry (String country) {
+        ArrayList<String> similar_data = new ArrayList<String>();
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(DATABASE2);
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30);
+            String query = "select name, date, max(newcases) cases, abs((select max(newcases) diff from countryrecords join country on country.id = countryrecords.countryid group by name) - max(newcases)) difference from countryrecords join country on country.id = countryrecords.countryid group by name order by abs((select max(newcases) from countryrecords join country on country.id = countryrecords.countryid where name = '" + country + "' group by name) - max(newcases)) asc limit 4";
+            ResultSet results = statement.executeQuery(query);
+            while (results.next()) {
+                String name     = results.getString("name");
+                String date = results.getString("date");
+                String cases = results.getString("cases");
+                similar_data.add(name);
+                similar_data.add(date);
+                similar_data.add(cases);
+            }
+            statement.close();
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+        return similar_data;
+    }   
+    public ArrayList<String> get3SimilarPeakCasesUSState (String state) {
+        ArrayList<String> similar_data = new ArrayList<String>();
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(DATABASE2);
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30);
+            String query = "select state.name n, date, max(newcases) cases, abs((select max (newcases) from staterecords join state on state.id = staterecords.stateid where state.name = '" + state + "' group by state.name) - max(newcases)) difference from staterecords join state on state.id = staterecords.stateid join country on country.id = state.countryid and country.name is 'US' group by state.name order by abs((select max(newcases) from staterecords join state on state.id = staterecords.stateid where state.name = '" + state + "' group by state.name) - max(newcases)) asc limit 4";
+            ResultSet results = statement.executeQuery(query);
+            while (results.next()) {
+                String name     = results.getString("n");
+                String date = results.getString("date");
+                String cases = results.getString("cases");
+                similar_data.add(name);
+                similar_data.add(date);
+                similar_data.add(cases);
+            }
+            statement.close();
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+        return similar_data;
+    }   
+    public ArrayList<String> get3SimilarPeakDeathsCountry (String country) {
+        ArrayList<String> similar_data = new ArrayList<String>();
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(DATABASE2);
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30);
+            String query = "select name, date, max(newdeaths) deaths, abs((select max (newdeaths) diff from countryrecords join country on country.id = countryrecords.countryid group by name) - max(newdeaths)) difference from countryrecords join country on country.id = countryrecords.countryid group by name order by abs((select max(newdeaths) from countryrecords join country on country.id = countryrecords.countryid where name = '" + country + "' group by name) - max(newdeaths)) asc limit 4";
+            ResultSet results = statement.executeQuery(query);
+            while (results.next()) {
+                String name     = results.getString("name");
+                String date = results.getString("date");
+                String deaths = results.getString("deaths");
+                similar_data.add(name);
+                similar_data.add(date);
+                similar_data.add(deaths);
+            }
+            statement.close();
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+        return similar_data;
+    }   
+    public ArrayList<String> get3SimilarPeakDeathsUSState (String state) {
+        ArrayList<String> similar_data = new ArrayList<String>();
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(DATABASE2);
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30);
+            String query = "select state.name n, date, max(newdeaths) deaths, abs((select max(newdeaths) from staterecords join state on state.id = staterecords.stateid where state.name = '" + state + "' group by state.name) - max(newdeaths)) difference from staterecords join state on state.id = staterecords.stateid join country on country.id = state.countryid and country.name is 'US' group by state.name order by abs((select max(newdeaths) from staterecords join state on state.id = staterecords.stateid where state.name = '" + state + "' group by state.name) - max(newdeaths)) asc limit 4";
+            ResultSet results = statement.executeQuery(query);
+            while (results.next()) {
+                String name     = results.getString("n");
+                String date = results.getString("date");
+                String deaths = results.getString("deaths");
+                similar_data.add(name);
+                similar_data.add(date);
+                similar_data.add(deaths);
+            }
+            statement.close();
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+        return similar_data;
     }   
 
 }
--- src/main/java/app/Similar.java
+++ src/main/java/app/Similar.java
@@ -5,66 +5,208 @@ import java.util.ArrayList;
 import io.javalin.http.Context;
 import io.javalin.http.Handler;
 
-/**
- * Temporary HTML as an example page.
- * 
- * Based on the Project Workshop code examples.
- * This page currently:
- *  - Provides a link back to the index page
- *  - Displays the list of movies from the Movies Database using the JDBCConnection
- *
- * @author Timothy Wiley, 2021. email: timothy.wiley@rmit.edu.au
- * @author Santha Sumanasekara, 2021. email: santha.sumanasekara@rmit.edu.au
- */
 public class similar implements Handler {
-
-    // URL of this page relative to http://localhost:7000/
+    
     public static final String URL = "/similar.html";
-
+    
     @Override
     public void handle(Context context) throws Exception {
-        String similarvar = "<html lang=\"en\">"+
-        "<head>"+
-        "    <meta charset=\"UTF-8\">"+
-        "    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">"+
-        "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">"+
-        "    <title>CovidWebsite</title>"+
-        "    <link rel=\"stylesheet\" href=\"main.css\">"+
-        "</head>"+
-        "<body>"+
-        "    <div class=\"navbar\">"+
-        "        <img id=\"mobile-cta\" class=\"mobile-menu\" src=\"menu.svg\" alt=\"Open Navigation\">"+
-        "        <nav>"+
-        "            <img id=\"mobile-exit\" class=\"mobile-menu-exit\" src=\"exit.svg\" alt=\"Close Navigation\">"+
-        "            <ul class='nav-ul'>"+
-        "                <li class='btn-lvl1'><a href=\"bigpicture.html\">Big Picture</a></li>"+
-        "                <li class='btn-lvl2'><a href=\"infections.html\">Infections</a></li>"+
-        "                <li class='btn-lvl2'><a href=\"deaths.html\">Deaths</a></li>"+
-        "                <li class='btn-lvl3'><a href=\"cumulative.html\">Cumulative</a></li>"+
-        "                <li class='btn-lvl3 highlight-red'><a href=\"similar.html\">Similar</a></li>"+
-        "            </ul>"+
-        "        </nav>"+
-        "    </div>"+
-        "    <script>"+
-        "        const mobileBtn = document.getElementById('mobile-cta')"+
-        "            nav = document.querySelector('nav');"+
-        "            mobileBtnExit = document.getElementById('mobile-exit');"+
-        ""+
-        "        mobileBtn.addEventListener('click', () => {"+
-        "            nav.classList.add('menu-btn');"+
-        "        })"+
-        ""+
-        "        mobileBtnExit.addEventListener('click', () => {"+
-        "            nav.classList.remove('menu-btn');"+
-        "        })"+
-        "    </script>"+
-        "</body>"+
+        String countryState;
+        String varName;
+        if (context.formParam("countryState") != null) {
+            countryState = context.formParam("countryState");
+        } else {
+            countryState = "country";
+        };
+        if (context.formParam("list") != null) {
+            varName = context.formParam("list");
+        } else {
+            varName = "Albania";
+        };
+        JDBC2             jdbc = new JDBC2();
+        ArrayList<String> country_names = jdbc.getCountryNames();
+        ArrayList<String> us_state_names = jdbc.getUSStateNames();
+        ArrayList<String> similar_casespermil;
+        ArrayList<String> similar_ratio;
+        ArrayList<String> similar_peak_deaths;
+        ArrayList<String> similar_peak_cases;
+        ArrayList<String> list;
+        if (countryState.equals("country")) {
+            list = country_names;
+            similar_casespermil = jdbc.get3SimilarCasesCountryMil(varName);
+            similar_ratio = jdbc.get3SimilarRatioCountry(varName);
+            similar_peak_deaths = jdbc.get3SimilarPeakDeathsCountry(varName);
+            similar_peak_cases = jdbc.get3SimilarPeakCasesCountry(varName);
+        } else {
+            list = us_state_names;
+            similar_casespermil = jdbc.get3SimilarCasesUSStateMil(varName);
+            similar_ratio = jdbc.get3SimilarRatioUSState(varName);
+            similar_peak_deaths = jdbc.get3SimilarPeakDeathsUSState(varName);
+            similar_peak_cases = jdbc.get3SimilarPeakCasesUSState(varName);
+        }
+        int i;
+        System.out.println(similar_ratio);
+        System.out.println(similar_peak_cases);
+        System.out.println(similar_peak_deaths);
+        // int               totalCases = jdbc.getPastYearTotalCasesCountry();
+        // String            countryName = jdbc.getPastMonthDeathsCountryTop1Name();
+        // int               countrySum = jdbc.getPastMonthDeathsCountryTop1Sum();
+        // int               totalSum = jdbc.getPastMonthDeathsCountryTotal();
+        // ArrayList<String> countryNames = jdbc.getPastWeekNoCasesCountry();
+        // int               count = jdbc.getPastWeekNoCasesStateCount();
+        // String            strCountryNames = "";
+        // for (String name : countryNames) {
+        //     strCountryNames = strCountryNames + name + ", ";
+        // }
+
+        String html = "<!DOCTYPE html>" +
+        "<html lang='en'>" +
+        "<head>" +
+        "    <title>Similar ❶❷❸</title>" +
+        
+        "    <meta charset='utf-8'>" +
+        "    <meta name='viewport' content='width=device-width, initial-scale=1'>" +
+        "    <meta name='author' content='@jinheock'>" +
+        "    <meta name='description' content='a webpage by @jinheock'>" +
+        "    <meta property='og:title' content='Similar ❶❷❸'>" +
+        "    <meta property='og:description' content='Similar ❶❷❸'>" +
+        "    <meta property='og:image' content='/some-image.png'>" +
+        "    <meta property='og:url' content='/this-page.html'>" +
+        "    <meta property='og:site_name' content='Similar ❶❷❸'>" +
+        "    <meta name='twitter:card' content='summary_large_image'>" +
+        "    <meta name='twitter:image:alt' content='image description'>" +
+        
+        "    <link href='reset.css' rel='stylesheet'>" +
+        "    <link href='style.css' rel='stylesheet'>" +
+        "    <link rel='icon' type='image/svg+xml' href='covid.svg'>" +
+        "</head>" +
+        "<body>" +
+        "<nav class='nav-topnav'>" +
+        "   <ul class='ul-topnav'>" +
+        "       <a href='bigpicture.html' class='a-topnav'>Big Picture" +
+        "       </a>" +
+        "       <a href='infections.html' class='a-topnav'>Infections" +
+        "       </a>" +
+        "       <a href='deaths.html' class='a-topnav'>Deaths" +
+        "       </a>" +
+        "       <a href='cumulative.html' class='a-topnav'>Cumulative" +
+        "       </a>" +
+        "       <a href='similar.html' class='a-topnav no-right-border a-topnav-highlight'>Similar" +
+        "       </a>" +
+        "   </ul>" +
+        "</nav>" +
+        "<main>" +
+        "    <h1>Covid-19</h1>" +
+        "    <form id='formSimilar' action='/similar.html' method='post'>" +
+        "               <select class='select-css' id='countryState' name='countryState' onChange='this.form.submit()'>" +
+        "               <option value='country' ";
+        if (countryState.equals("country")) {
+            html = html + "selected>Countries</option>";
+        } else {
+            html = html + ">Countries</option>";
+        }
+        html = html +
+        "               <option value='state' ";
+        if (countryState.equals("state")) {
+            html = html + "selected>U.S. States</option>";
+        } else {
+            html = html + ">U.S. States</option>";
+        }
+        html = html +
+        "               </select> " +
+        " <label for ='countryState'>Find Similar: </label>" +
+        "               <select class='select-css' id='list' name='list' onChange='this.form.submit()'>";
+        // "               <option value='' selected='' disabled=''>Choose Country</option>";
+        
+        for (String name : list) {
+            if (name.equals(varName)) {
+                html = html + "<option selected value='" + name + "'>" + name + "</option>";
+            } else {
+                html = html + "<option value='" + name + "'>" + name + "</option>";
+            }
+        }
+        html = html +
+        "               </select>" +
+        "           </form>" +
+        "           <div class='div-tables'>" +
+        "       <table>" +
+        "       <thead>" +
+        "           <tr>" +
+        "               <th>Country</th>" +
+        "               <th>Infections<br><small>per million ppl</small></th>" +
+        "           </tr>" +
+        "       </thead>" +
+        "       <tbody>";
+        for (i = 0; i < 3; i++) {
+            html = html +
+            "<tr>" +
+            "<td>" + similar_casespermil.get(i*2) + "</td>" +
+            "<td>" + similar_casespermil.get((i*2)+1) + "</td>" +
+            "</tr>";
+        }
+        html = html +
+        "       </tbody></table>" +
+        "       <table>" +
+        "       <thead>" +
+        "           <tr>" +
+        "               <th>Country</th>" +
+        "               <th>Ratio<br><small>deaths/infection</small></th>" +
+        "           </tr>" +
+        "       </thead>" +
+        "       <tbody>";
+        for (i = 0; i < 3; i++) {
+            html = html +
+            "<tr>" +
+            "<td>" + similar_ratio.get(i*2) + "%</td>" +
+            "<td>" + similar_ratio.get((i*2)+1) + "%</td>" +
+            "</tr>";
+        }
+        html = html +
+        "       </tbody></table>" +
+        "       <table>" +
+        "       <thead>" +
+        "           <tr>" +
+        "               <th>Country</th>" +
+        "               <th>Peak Deaths<br><small>highest number of new<br>deaths in 1 day</small></th>" +
+        "           </tr>" +
+        "       </thead>" +
+        "       <tbody>";
+        for (i = 0; i < 4; i++) {
+            html = html +
+            "<tr>" +
+            "<td>" + similar_peak_deaths.get(i*3) + "</td>" +
+            "<td>" + similar_peak_deaths.get((i*3)+2) + "<br><small>" + similar_peak_deaths.get((i*3)+1) + "</small></td>" +
+            "</tr>";
+        }
+        html = html +
+        "       </tbody></table>" +
+        "       <table>" +
+        "       <thead>" +
+        "           <tr>" +
+        "               <th>Country</th>" +
+        "               <th>Peak Cases<br><small>highest number of new<br>cases in 1 day</small></th>" +
+        "           </tr>" +
+        "       </thead>" +
+        "       <tbody>";
+        for (i = 0; i < 4; i++) {
+            html = html +
+            "<tr>" +
+            "<td>" + similar_peak_cases.get(i*3) + "</td>" +
+            "<td>" + similar_peak_cases.get((i*3)+2) + "<br><small>" + similar_peak_deaths.get((i*3)+1) + "</small></td>" +
+            "</tr>";
+        }
+        html = html +
+        "       </tbody></table>" +
+        "</main>" +
+        "    <script src='litty.js'></script>" +
+        "</body>" +
         "</html>";
-
-
-        // DO NOT MODIFY THIS
+            
+        
+            
+// DO NOT MODIFY THIS
         // Makes Javalin render the webpage
-        context.html(similarvar);
+        context.html(html);
     }
 
 }
